683fe45433ed2a414178268ab6f8b62fef5073d9
Sita Tailor <sitatailor@hotmail.co.uk>
2023-12-13 15:57:00 +0000

    created entire data pipeline to write results to s3 bucket for each protein

--- docker_project/entrypoint.sh
+++ docker_project/entrypoint.sh
@@ -3,5 +3,18 @@
 # Activate the Conda environment
 source activate myenv
 
-# Execute the Python script with provided arguments
-python -u pipeline_script.py "$@"
+# Assign protein ID provided as arguement
+protein_id="$1"
+
+# Fetch the fasta entry for prtein ID from S3
+fasta_entry=$(aws s3 cp s3://comp0235-ucabst7/human_proteome - | awk -v RS=">" -v id="$protein_id" '$1 == id {print ">" $0}')
+
+# run pipeline_script.py with obtanied fasta file entry from the protein ID
+echo "$fasta_entry" > temp.fasta
+python -u pipeline_script.py temp.fasta
+
+# Convert hhr_parse.out to CSV with selected columns using awk
+awk 'BEGIN {FS=","; OFS=","} {print $1, $2, $6, $7}' hhr_parse.out > "${protein_id}_result.csv"
+
+# Upload the CSV file to S3 with a unique name
+aws s3 cp "${protein_id}_result.csv" s3://comp0235-ucabst7/processing_results/
