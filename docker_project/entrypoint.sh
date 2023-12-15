0c5d920afb894b49b5eb222bfe911f656f8c7c5c
Sita Tailor <sitatailor@hotmail.co.uk>
2023-12-15 16:05:10 +0000

    final updates before kuburnetes cluster initialisation

--- docker_project/entrypoint.sh
+++ docker_project/entrypoint.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-# Activate the Conda environment
+# activate conda environment
 source activate myenv
 
-# Assign protein ID provided as arguement
-protein_id="$1"
+# function - process a single protein ID
+process_protein_id() {
+    local protein_id="$1"
 
-# Fetch the fasta entry for prtein ID from S3
-fasta_entry=$(aws s3 cp s3://comp0235-ucabst7/human_proteome - | awk -v RS=">" -v id="$protein_id" '$1 == id {print ">" $0}')
+    # get fasta entry for protein ID from S3 bucket
+    fasta_entry=$(aws s3 cp s3://comp0235-ucabst7/human_proteome - | awk -v RS=">" -v id="$protein_id" '$1 == id {print ">" $0}')
 
-# run pipeline_script.py with obtanied fasta file entry from the protein ID
-echo "$fasta_entry" > temp.fasta
-python -u pipeline_script.py temp.fasta
+    # run pipeline_script.py with fasta file entry for the protein ID
+    echo "$fasta_entry" > temp.fasta
+    python -u pipeline_script.py temp.fasta
 
-# Convert hhr_parse.out to CSV with selected columns using awk
-awk 'BEGIN {FS=","; OFS=","} {print $1, $2, $6, $7}' hhr_parse.out > "${protein_id}_result.csv"
+    # convert hhr_parse.out to CSV with selected columns (query_id, best_hit,score_std,score_gmean)  using awk
+    awk 'BEGIN {FS=","; OFS=","} {print $1, $2, $6, $7}' hhr_parse.out > "${protein_id}_result.csv"
 
-# Upload the CSV file to S3 with a unique name
-aws s3 cp "${protein_id}_result.csv" s3://comp0235-ucabst7/processing_results/
+    # upload CSV file to S3 with a unique name
+    aws s3 cp "${protein_id}_result.csv" s3://comp0235-ucabst7/processing_results/
+}
+
+# read the batch file containing protein IDs
+batch_file="$1"
+
+# process each protein ID in the batch file
+while IFS= read -r protein_id; do
+    process_protein_id "$protein_id" &
+done < "$batch_file"
+
+# wait for all background jobs to finish
+wait
+
